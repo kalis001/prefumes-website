df49ea4d1e6af9ad2fbf771b537404503e35d9ec
kalis001 <kaliswarank171@gmail.com>
2025-02-10 14:32:57 +0530

    new update

--- Head.js
+++ Head.js
@@ -8,3 +8,6 @@ const close_btn = document.getElementById('close')
 close_btn.addEventListener('click', () => {
     navLinks.classList.remove('active');
 });
+function myFunction() {
+    var x = document.getElementById("myEmail").pattern;
+}
